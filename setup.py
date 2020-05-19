da06352fb1b7b8fe179ff284836f0110e6a22e92
duncan_mearns <duncan.mearns@gmail.com>
2020-05-19 17:51:28 +0200

    Added url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Basic computer vision library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    url="https://github.com/DuncanMearns/video_analysis_toolbox",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
