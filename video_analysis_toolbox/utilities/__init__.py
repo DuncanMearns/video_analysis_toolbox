b48a0f8d6ff0cbd770c596052fa2236109dee038
duncan_mearns <duncan.mearns@gmail.com>
2020-05-19 18:22:17 +0200

    Created TrackingError

--- video_analysis_toolbox/utilities/__init__.py
+++ video_analysis_toolbox/utilities/__init__.py
@@ -1,1 +1,2 @@
 from .keyboard_interaction import KeyboardInteraction
+from . data_structures import feature_vector, TrackingError
--- video_analysis_toolbox/utilities/data_structures.py
+++ video_analysis_toolbox/utilities/data_structures.py
@@ -1,5 +1,11 @@
 from collections import namedtuple
 
 
+class TrackingError(Exception):
+    """An error that can be raised if any problem is detected in tracking"""
+    def __init__(self):
+        super().__init__()
+
+
 # Feature vector for storing the x_position, y_position and orientation of an object
 feature_vector = namedtuple('feature_vector', ('x', 'y', 'angle'))
