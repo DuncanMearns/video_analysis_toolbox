ef7bb20b1ad65aba76e694389dd04ddbfeca0650
duncan_mearns <duncan.mearns@gmail.com>
2020-05-19 18:43:30 +0200

    Fixed TrackingError class

--- video_analysis_toolbox/utilities/data_structures.py
+++ video_analysis_toolbox/utilities/data_structures.py
@@ -1,10 +1,18 @@
 from collections import namedtuple
 
 
-class TrackingError(Exception):
+class Error(Exception):
+    """Base class"""
+    pass
+
+
+class TrackingError(Error):
     """An error that can be raised if any problem is detected in tracking"""
-    def __init__(self):
-        super().__init__()
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
 
 
 # Feature vector for storing the x_position, y_position and orientation of an object
